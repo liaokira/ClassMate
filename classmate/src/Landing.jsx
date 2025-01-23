6eb696217bcf78f707b6ff6ad011d751b0fd9b07
4disease <kirayliao@gmail.com>
2025-01-22 16:41:34 -0800

    Commit base framework

---
+++ classmate/src/Landing.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Landing.css'
+
+function Landing() {
+
+  return (
+    <>
+      <h1>Welcome to ClassMate!</h1>
+      <h2>This is the Landing Page.</h2>
+    </>
+  )
+}
+
+export default Landing
