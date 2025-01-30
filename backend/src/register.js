8f4a32486f3ef5123299659bb9d57d974db38259
Shreyas0110 <shreyas_gupta@outlook.com>
2025-01-29 20:16:22 -0800

    postgres docker kinda works

--- backend/src/register.js
+++ backend/src/register.js
@@ -5,11 +5,11 @@ const secrets = require('./secrets');
 
 const {Pool} = require('pg');
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  host: 'db',
+  port: '5432',
+  database: 'example',
+  user: 'postgres',
+  password: 'test',
 });
 
 exports.register = async (req, res) => {
