9a6462003120f7502bce7ce5785dccb5162c3def
Shreyas0110 <shreyas_gupta@outlook.com>
2025-02-09 23:57:51 -0800

    something is broken idk what

--- backend/src/profile.js
+++ backend/src/profile.js
@@ -12,9 +12,9 @@ const pool = new Pool({
   password: 'test',
 });
 
-//NO IDEA HOW TO USER AUTHENTICATE
+
 exports.getProfile = async (req, res) => {
-  const {id} = req.body;
+  const id = req.params.id;
   const userSelect = `SELECT (bio_data, full_name) FROM member_profiles WHERE id = $1`;
   const userQuery = {
     text: userSelect,
@@ -22,7 +22,7 @@ exports.getProfile = async (req, res) => {
   };
   const {rows} = await pool.query(userQuery);
   if (rows.length) {
-      res.status(200).json({bio: rows[0].bio_data, full_name: rows[0].full_name});
+      res.status(200).json({id: id, bio: rows[0].bio_data, full_name: rows[0].full_name});
   }
   else {
     const userSelect2 = `SELECT (id) FROM member WHERE id = $1`;
@@ -32,22 +32,39 @@ exports.getProfile = async (req, res) => {
     };
     const {rows2} = await pool.query(userQuery2);
     if (rows2.length){
-      res.status(200).json({bio: '', full_name: ''});
+      res.status(200).json({id: id, bio: '', full_name: ''});
     }
     else{
-      res.status(401).send('Invalid credentials');
+      res.status(404).send('Error: No Profile for user found');
     }
   }
 };
 
-exports.check = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, secrets.accessToken, (err, user) => {
-    if (err) {
-      return res.sendStatus(403);
+exports.setProfile = async (req, res) => {
+  const {id, full_name, bio} = req.body;
+  const userINSERT = `INSERT INTO member_profiles(id, bio_data, full_name) 
+                      VALUES ($1, $2, $3) 
+                      ON CONFLICT (id) DO UPDATE SET 
+                        bio_data = EXCLUDED.bio_data,
+                        full_name = EXCLUDED.full_name
+                      RETURNING id`;
+  const userQuery = {
+    text: userINSERT,
+    values: [`${id}`, `${bio}`, `${full_name}`],
+  };
+  const {rows} = await pool.query(userQuery);
+  if (rows.length) {
+    /*
+    if(rows[0].xmax === 0){
+      res.status(200).json({id});
     }
-    req.user = user;
-    next();
-  });
+    else{
+      res.status(201).json({id});
+    }*/
+    res.status(200).json({id});
+  }
+  else {
+    res.status(404).send('Error: No Profile for User Found');
+  }
 };
+
