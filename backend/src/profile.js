0f83a2893645a5aa31451e12b4aceba39c9d2467
Shreyas0110 <shreyas_gupta@outlook.com>
2025-02-10 19:17:33 -0800

    profile works mostly

--- backend/src/profile.js
+++ backend/src/profile.js
@@ -15,23 +15,26 @@ const pool = new Pool({
 
 exports.getProfile = async (req, res) => {
   const id = req.params.id;
-  const userSelect = `SELECT (bio_data, full_name) FROM member_profiles WHERE id = $1`;
+  const userSelect = `SELECT * FROM member_profiles WHERE id = $1`;
   const userQuery = {
     text: userSelect,
     values: [`${id}`],
   };
   const {rows} = await pool.query(userQuery);
   if (rows.length) {
-      res.status(200).json({id: id, bio: rows[0].bio_data, full_name: rows[0].full_name});
+    console.log("rows", rows);
+    console.log("bio data", rows[0].bio_data);
+    console.log("name data", rows[0].full_name);
+    res.status(200).json({id: id, bio: rows[0].bio_data, full_name: rows[0].full_name});
   }
   else {
-    const userSelect2 = `SELECT (id) FROM member WHERE id = $1`;
+    const userSelect2 = `SELECT id FROM member WHERE id = $1`;
     const userQuery2 = {
       text: userSelect2,
       values: [`${id}`],
     };
     const {rows2} = await pool.query(userQuery2);
-    if (rows2.length){
+    if (rows2 && rows2.length){
       res.status(200).json({id: id, bio: '', full_name: ''});
     }
     else{
@@ -47,20 +50,24 @@ exports.setProfile = async (req, res) => {
                       ON CONFLICT (id) DO UPDATE SET 
                         bio_data = EXCLUDED.bio_data,
                         full_name = EXCLUDED.full_name
-                      RETURNING id`;
+                      RETURNING id, bio_data, full_name`;
   const userQuery = {
     text: userINSERT,
     values: [`${id}`, `${bio}`, `${full_name}`],
   };
   const {rows} = await pool.query(userQuery);
   if (rows.length) {
-    /*
+    
     if(rows[0].xmax === 0){
       res.status(200).json({id});
     }
     else{
       res.status(201).json({id});
-    }*/
+    }
+      console.log("id", rows[0].id);
+
+      console.log("bio data", rows[0].bio_data);
+      console.log("name data", rows[0].full_name);
     res.status(200).json({id});
   }
   else {
