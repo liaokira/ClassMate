9877377482230688223b8208263409044b3b4db7
Michael Tan <0823amichael2015@gmail.com>
2025-02-25 21:33:59 -0800

    linked classes to study group

--- backend/sql/init/schema.sql
+++ backend/sql/init/schema.sql
@@ -26,6 +26,7 @@ CREATE TABLE study_groups(
     id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
     group_name VARCHAR(18) NOT NULL,
     group_description TEXT NOT NULL DEFAULT 'Add a group description...'
+    associated_class REFERENCES classes(id)
 );
 
 DROP TABLE IF EXISTS member_profiles CASCADE;
