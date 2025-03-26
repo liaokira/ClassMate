585c9102dbb8a1f0486213ed5920a797fec9ca57
4disease <kirayliao@gmail.com>
2025-03-25 20:05:33 -0700

    some new stuff

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ echo "🔧 Installing dependencies..."
 echo "🐳 Starting Docker (Backend)..."
 (cd backend && docker-compose up --build -d)
 
-echo "📦 Resetting the database..."
-(cd backend && ./init_db.sh)  # Modify if needed
+# echo "📦 Resetting the database..."
+# (cd backend && ./init_db.sh)  # Modify if needed
 
-echo "🚀 Starting Frontend..."
-(cd frontend && npm run dev)
+echo "📦 Building the frontend..."
+(cd frontend && npm run build)
